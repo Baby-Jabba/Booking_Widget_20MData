21a4b9549dab601ca3d306d335dc581edcbf20c6
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-02-22 16:43:24 -0800

    ready to depoly

--- database/seeder/postgreSQLSeed.js
+++ database/seeder/postgreSQLSeed.js
@@ -10,7 +10,7 @@ const client = new Client({
 client.connect();
 
 const sites1 = () => {
-  for (let index = 0; index < 3333333; index++) {
+  for (let index = 0; index < 3333334; index++) {
     let siteName = `${faker.company.catchPhraseAdjective()}.com`;
     let logo = faker.image.cats();
     let tweak = faker.finance.amount(0.8, 1, 2);
@@ -28,25 +28,6 @@ const sites1 = () => {
   }
 };
 const sites2 = () => {
-  for (let index = 0; index < 3333333; index++) {
-    let siteName = `${faker.company.catchPhraseAdjective()}.com`;
-    let logo = faker.image.cats();
-    let tweak = faker.finance.amount(0.8, 1, 2);
-    let incentive = faker.random.number({ min: 0, max: 2 });
-    let params = [siteName, logo, tweak, incentive];
-    client
-      .query(
-        'INSERT INTO sites(site_name, logo, tweak, incentive) VALUES($1,$2,$3,$4)',
-        params
-      )
-      .then(res => {
-        console.log(`${index}sites added;`);
-      })
-      .catch(e => console.error(e.stack));
-  }
-};
-
-const sites3 = () => {
   for (let index = 0; index < 3333334; index++) {
     let siteName = `${faker.company.catchPhraseAdjective()}.com`;
     let logo = faker.image.cats();
@@ -65,8 +46,27 @@ const sites3 = () => {
   }
 };
 
+const sites3 = () => {
+  for (let index = 0; index < 3333336; index++) {
+    let siteName = `${faker.company.catchPhraseAdjective()}.com`;
+    let logo = faker.image.cats();
+    let tweak = faker.finance.amount(0.8, 1, 2);
+    let incentive = faker.random.number({ min: 0, max: 2 });
+    let params = [siteName, logo, tweak, incentive];
+    client
+      .query(
+        'INSERT INTO sites(site_name, logo, tweak, incentive) VALUES($1,$2,$3,$4)',
+        params
+      )
+      .then(res => {
+        console.log(`${index}sites added;`);
+      })
+      .catch(e => console.error(e.stack));
+  }
+};
+
 const hotels1 = () => {
-  for (let index = 0; index < 3333333; index++) {
+  for (let index = 0; index < 3333334; index++) {
     const standardRate = faker.random.number({ min: 200, max: 1200 });
     const adultPremium = faker.finance.amount(0.1, 0.2, 2);
     const childPremium = faker.finance.amount(0.05, 0.1, 2);
@@ -87,7 +87,7 @@ const hotels1 = () => {
   }
 };
 const hotels2 = () => {
-  for (let index = 0; index < 3333333; index++) {
+  for (let index = 0; index < 3333334; index++) {
     const standardRate = faker.random.number({ min: 200, max: 1200 });
     const adultPremium = faker.finance.amount(0.1, 0.2, 2);
     const childPremium = faker.finance.amount(0.05, 0.1, 2);
@@ -109,7 +109,7 @@ const hotels2 = () => {
 };
 
 const hotels3 = () => {
-  for (let index = 0; index < 3333334; index++) {
+  for (let index = 0; index < 3333335; index++) {
     const standardRate = faker.random.number({ min: 200, max: 1200 });
     const adultPremium = faker.finance.amount(0.1, 0.2, 2);
     const childPremium = faker.finance.amount(0.05, 0.1, 2);
@@ -159,9 +159,9 @@ seedDates = () => {
   );
 };
 
-sites1();
-// sites2();
-// sites3();
+//sites1();
+sites2();
+sites3();
 // hotels1();
 // hotels2();
 // hotels3();
@@ -169,8 +169,8 @@ seedDates();
 
 //    CREATE TABLE sites (
 //     id serial PRIMARY KEY NOT NULL,
-//     site_name CHAR (50),
-//     logo char (25),
+//     site_name CHAR (500),
+//     logo char (250),
 //     tweak decimal,
 //     incentive decimal
 //     );
@@ -181,7 +181,7 @@ seedDates();
 //   adult_premium decimal,
 //   child_premium decimal,
 //   max_stay decimal,
-//   deal char(25)
+//   deal char(250)
 // );
 
 // CREATE TABLE dates (
