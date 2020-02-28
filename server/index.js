d93aa2be7b9d2bedd1b04c636dc61c47fddc4ca6
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-02-27 19:50:01 -0800

    fixed  bugs

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
 });
 app.use('/bundle.js', express.static(__dirname + '/../public/bundle.js'));
 app.use(
-  '/loaderio-c4f7aaa46044d73a9734101b127559e4.txt',
+  'loaderio-c4f7aaa46044d73a9734101b127559e4.txt',
   express.static(
     __dirname + '/../public/loaderio-c4f7aaa46044d73a9734101b127559e4.txt'
   )
