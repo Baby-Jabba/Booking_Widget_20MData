c8ea0c5b36272f29d043f09515ca16708faf5e2b
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-02-27 19:58:03 -0800

    fixed  bugs

--- server/index.js
+++ server/index.js
@@ -13,18 +13,14 @@ app.use(function(req, res, next) {
   next();
 });
 app.use('/bundle.js', express.static(__dirname + '/../public/bundle.js'));
-// app.use(
-//   'loaderio-c4f7aaa46044d73a9734101b127559e4.txt',
-//   express.static(
-//     __dirname + '/../public/loaderio-c4f7aaa46044d73a9734101b127559e4.txt'
-//   )
-// );
-app.use(express.urlencoded({ extended: false }));
-app.use('/:id', express.static('./public'));
 app.use(
   '/loaderio-c4f7aaa46044d73a9734101b127559e4.txt',
-  express.static('/../public/loaderio-c4f7aaa46044d73a9734101b127559e4.txt')
+  express.static(
+    __dirname + '/../public/loaderio-c4f7aaa46044d73a9734101b127559e4.txt'
+  )
 );
+app.use(express.urlencoded({ extended: false }));
+app.use('/:id', express.static('./public'));
 
 const PORT = process.env.BOOKING_PORT || 50003;
 
